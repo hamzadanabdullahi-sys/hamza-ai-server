dec22023f2fd33312bacf7418f8372fd699b06ff
hamzadanabdullahi-sys <hamzadanabdullahi@gmail.com>
2026-06-06 18:25:51 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hamza AI Server is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
